abbbc0539640e8cb1e87252e9045d311e4c1b891
aryangurau <aryangurau143@gmail.com>
2024-12-04 19:07:24 +0545

    added slider carousel banner

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -1,15 +1,39 @@
+
+import { assets } from "../../assets/assets";
 import "./Header.css";
 
 const Header = () => {
+  const menu = () => {
+    alert("menu is under maintenance");
+  };
   return (
     <div className="header">
+      <div className="slider">
+        <div className="slides">
+          {/* Slide 1 */}
+          <div className="slide">
+            <img src={assets.retro} alt="" />
+           
+          </div>
+          {/* Slide 2 */}
+          <div className="slide">
+          <img src={assets.banner2} alt="" />
+          </div>
+          {/* Slide 3 */}
+          <div className="slide">
+          <img src={assets.header_banner} alt="" />
+           
+          </div>
+        </div>
+      </div>
+
       <div className="header-contents">
         <h2>Order your favourite food here</h2>
         <p>
           Choose from a diverse menu featuring a delectable array of dishes
           crafter with the finest ingredients
         </p>
-        <button>View Menu</button>
+        <button onClick={menu}>View Menu</button>
       </div>
     </div>
   );
