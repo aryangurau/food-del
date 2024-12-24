2411f6329f415a9531080d53431440afe16e05b6
aryangurau <aryangurau143@gmail.com>
2024-12-24 17:53:41 +0545

    modified the frontend url

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -7,7 +7,7 @@ import Stripe from "stripe";
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 const placeOrder = async (req, res) => {
-  const frontend_url = "https://food-del-frontend-ex9y.onrender.com";
+  const frontend_url = "http://localhost:5173";
   try {
     console.log("Request body:", req.body);
 
