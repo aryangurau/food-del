d34aeb22c2a21a9a5175c1cde4b57bf4fb2d1c48
aryangurau <aryangurau143@gmail.com>
2024-12-24 17:51:21 +0545

    added admin dashbaord

--- admin/src/components/SIdebar/Sidebar.jsx
+++ admin/src/components/SIdebar/Sidebar.jsx
@@ -5,6 +5,10 @@ const Sidebar = () => {
   return (
     <div className="sidebar">
       <div className="sidebar-options">
+      <NavLink to="/dashboard" className="sidebar-option">
+          <img src={assets.add_icon} alt="" />
+          <p>Dashboard</p>
+        </NavLink>
         <NavLink to="/add" className="sidebar-option">
           <img src={assets.add_icon} alt="" />
           <p>Add Items</p>
