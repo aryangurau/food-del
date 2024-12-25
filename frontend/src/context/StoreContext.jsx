2de0204f3875d5ab51049dfcc97aa18a7c44786a
aryangurau <aryangurau143@gmail.com>
2024-12-25 21:51:43 +0545

    decoding token from local storage

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -7,7 +7,8 @@ export const StoreContext = createContext(null);
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
   const url = "http://localhost:4000";
-  const [token, setToken] = useState("");
+  const [token, setToken] = useState(localStorage.getItem('token'));
+  // const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
   const addToCart = async (itemId) => {
