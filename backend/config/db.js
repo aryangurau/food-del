8cb1edfcf32ebc12be83e47a78c73456c43f8702
aryangurau <aryangurau143@gmail.com>
2025-05-19 15:04:45 +0545

    database code optimization

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
 
-export const connectDB = async()=>{
-    await mongoose.connect("mongodb+srv://aryangurau143:9809241661@cluster0.c8416.mongodb.net/food-del").then(()=>console.log("DB conected"));
-}
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGODB_URI || "mongodb+srv://aryangurau143:9809241661@cluster0.c8416.mongodb.net/food-del");
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
